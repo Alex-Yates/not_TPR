789dded8a14191bc431969cd59cb4fc8f404988f
Alex Yates <Alex-Yates@users.noreply.github.com>
2023-06-16 14:17:52 +0100

    Dml aware migrate (#7)
    * do not commit the report files back to source control
    
    * Migrate works, and looks cleaner, but DDL scripts marked as DML are incorrectly passing. Not sure why.
    
    * Migrate now uses the appropriate DML user and fails for DDL scripts added anywhere other than the DDL directory
    
    * removing testForDDL as now baked into migrate anyway
    
    * removing unnecessary logging
    
    * Cleaning up the code
    
    * cleaning up the comments

--- build/migrate.ps1
+++ build/migrate.ps1
@@ -3,49 +3,124 @@ param (
     $licenceKey = ""
 )
 
+# If any part of the script fails, stop
 $ErrorActionPreference = "stop"
 
 # Redgate telemetry slows things down a lot. Disabling it for speed.
-& setx REDGATE_DISABLE_TELEMETRY true
+& setx REDGATE_DISABLE_TELEMETRY true | out-null 
 
+# Managing relative paths to all the necessary files is a pain
 $thisScript = $MyInvocation.MyCommand.Path
 $buildDir = Split-Path $thisScript -Parent
 $gitRoot = Split-Path $buildDir -Parent
 $fullyQualifiedFlywayRoot = "$gitRoot\$flywayRoot"
+
+# Importing some dependencies
 Write-Output "Importing helper functions from $buildDir\functions.psm1."
 import-module "$buildDir\functions.psm1"
+Write-Output "Importing module dbatools."
+import-module dbatools
 
+# Using a few functions from $buildDir\functions.psm1 to grab some required info from the flyway.conf file
+$flywayHistoryDataScript = Get-FlywaySchemaHistoryDataScriptPath -FlywayRoot $fullyQualifiedFlywayRoot
 $jdbcUrl = Get-JdbcUrl -flywayRoot $flywayRoot
-
 $server = Get-ServerFromJdbcUrl $jdbcUrl
 $instance = Get-InstanceFromJdbcUrl $jdbcUrl
 $database = Get-DatabaseFromJdbcUrl $jdbcUrl
-
-Write-Output "Given parameters:"
-Write-Output "  flywayRoot: $flywayRoot"
-
-$locations = "filesystem:$gitRoot\$flywayRoot\migrations"
 $serverInstance = $server
 if ($instance -notlike ""){
     $serverInstance = "$server\$instance"
 }
-$flywayHistoryDataScript = Get-FlywaySchemaHistoryDataScriptPath -FlywayRoot $fullyQualifiedFlywayRoot
 
+# Logging a bunch of parameters for convenience/troubleshooting
+Write-Output "Given parameters:"
+Write-Output "- flywayRoot: $flywayRoot"
 Write-Output "Derived parameters:"
-Write-Output "  jdbcUrl:        $jdbcUrl"
-Write-Output "  server:         $server"
-Write-Output "  instance:       $instance"
-Write-Output "  database:       $database"
-Write-Output "  thisScript:     $thisScript"
-Write-Output "  gitRoot:        $gitRoot"
-Write-Output "  buildDir:       $buildDir"
-Write-Output "  locations:      $locations"
-Write-Output "  serverInstance: $serverInstance"
-Write-Output "  fullyQualifiedFlywayRoot: $fullyQualifiedFlywayRoot"
-Write-Output "  flywayHistoryDataScript:  $flywayHistoryDataScript"
-
-Write-Output "Running Flyway migrate with the following command:"
-Write-Output "  & flyway migrate -url=""$jdbcUrl"" -locations=""$locations"" -licenseKey=""$licenceKey"" -errorOverrides=""S0001:0:I-""" 
+Write-Output "- jdbcUrl:        $jdbcUrl"
+Write-Output "- server:         $server"
+Write-Output "- instance:       $instance"
+Write-Output "- database:       $database"
+Write-Output "- thisScript:     $thisScript"
+Write-Output "- gitRoot:        $gitRoot"
+Write-Output "- buildDir:       $buildDir"
+Write-Output "- serverInstance: $serverInstance"
+Write-Output "- fullyQualifiedFlywayRoot: $fullyQualifiedFlywayRoot"
+Write-Output "- flywayHistoryDataScript:  $flywayHistoryDataScript"
 Write-Output ""
-& flyway migrate -url=""$jdbcUrl"" -locations=""$locations"" -licenseKey=""$licenceKey"" -errorOverrides=""S0001:0:I-""
 
+Write-Output "Creating a sql login and user on $database with only DML access."
+# Generate a random password for the DML user. We don't want any humans using this login!
+[Reflection.Assembly]::LoadWithPartialName("System.Web") | out-null
+do {
+    $randomString = [System.Web.Security.Membership]::GeneratePassword(15,2) # Generates a random 15 char password with 2 special chars
+} until ($randomString -match '\d')                                          # GeneratePassword does not guarantee a number character. If no numbers, try again.
+$dmlLoginPassword = ConvertTo-SecureString $randomString -AsPlainText -Force # Using a secure string to avoid logging plaintext password in public logs
+# A SQL script to create a dmlChecker login/user
+$createDmlUserSql = @"
+USE $database;
+IF NOT EXISTS (SELECT * FROM sys.syslogins WHERE name = 'dmlChecker')
+BEGIN
+    CREATE LOGIN dmlChecker WITH PASSWORD = '$dmlLoginPassword';
+END
+CREATE USER dmlChecker FOR LOGIN dmlChecker;
+GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::dbo TO dmlChecker;
+"@
+# If login/user already exists, delete, so we can recreate fresh, with known password and expected permissions.
+Remove-DbaDbUser -SqlInstance $serverInstance -User dmlChecker | out-null
+Remove-DbaLogin -SqlInstance $serverInstance -Login dmlChecker -Force | out-null
+# Create login and user, using tsql script above
+Invoke-DbaQuery -SqlInstance $serverInstance -Query $createDmlUserSql
+Write-Output ""
+
+# Using "flyway info" command to query flyway_schema_history and source code to get a list of pending migrations and infer whether they are listed for DML or DDL.
+Write-Output "Running flyway info to discover pending migrations:"
+Write-Output "- Executing: "
+Write-Output "    & flyway info"
+Write-Output "        -workingDirectory=""$gitRoot/$flywayRoot"""
+Write-Output "        -configFiles=""$gitRoot/$flywayRoot/flyway.conf"""
+Write-Output "        -outputType=""Json"""
+Write-Output "        -licenseKey=***"
+$flywayInfo = (& flyway info -workingDirectory="$gitRoot/$flywayRoot" -configFiles="$gitRoot/$flywayRoot/flyway.conf" -outputType="Json" -licenseKey="$licenceKey") | ConvertFrom-Json
+$currentVersion = $flywayInfo.schemaVersion
+Write-Output "- CurrentVersion is: $currentVersion"
+$allMigrations = $flywayInfo.migrations
+$pendingMigrations = $allMigrations | Where-Object {$_.state -like "Pending"}
+Write-Output "- Pending migrations are:"
+Write-Output $pendingMigrations | Format-Table -Property version, description, state, filepath
+Write-Output ""
+
+# Running flyway migrate one migration at a time, using the default user for DDL, but the DML user, with restricted access, for DML
+Write-Output "Running each pending script against the scratch database, with DML scripts executed as a user with only DML permissions."
+$dmlUrl = $jdbcUrl.replace(";integratedSecurity=true",";integratedSecurity=false;user=dmlChecker;password=$dmlLoginPassword")
+$pendingMigrations | ForEach-Object {
+    $thisVersion = $_.version
+    $thisDescription = $_.description
+    $isDmlOnly = $true
+    if ($_.filepath -like "*\$flywayRoot\migrations\DDL\*"){
+        $isDmlOnly = $false
+    }
+    $thisUrl = $jdbcUrl
+    $versionNumberAndDescriptionAsText = "$thisVersion" + ": $thisDescription"
+    Write-Output "- Upgrading to $versionNumberAndDescriptionAsText"
+    if ($isDmlOnly){
+        $thisUrl = $dmlUrl
+        Write-Output "  - $thisVersion should NOT contain DDL. Using a DML only login."
+    }
+    else {
+        Write-Output "  - $thisVersion may contain contain DDL. Using default login."
+    }
+    Write-Output "- Executing: "
+    Write-Output "    & flyway migrate"
+    Write-Output "        -workingDirectory=""$gitRoot/$flywayRoot"""
+    Write-Output "        -configFiles=""$gitRoot/$flywayRoot/flyway.conf"""
+    Write-Output "        -url=""$thisUrl"""
+    Write-Output "        -target=""$thisVersion"""
+    Write-Output "        -licenseKey=***"
+    & flyway migrate -workingDirectory="$gitRoot/$flywayRoot" -configFiles="$gitRoot/$flywayRoot/flyway.conf" -url="$thisUrl" -target="$thisVersion" -licenseKey="$licenceKey"
+}
+Write-Output ""
+
+# Cleaning up the DML only login and user. We don't need them any more.
+Write-Output "Removing dmlChecker user and login."
+Remove-DbaDbUser -SqlInstance $serverInstance -User dmlChecker | out-null 
+Remove-DbaLogin -SqlInstance $serverInstance -Login dmlChecker -Force | out-null 
